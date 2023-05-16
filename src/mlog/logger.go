4e30f96427c2337cc58058e48054430eed4abc07
Saurav Suman <sauravsuman@Sauravs-Air.bbrouter>
2023-05-16 19:10:59 +0530

    improved logs

--- src/mlog/logger.go
+++ src/mlog/logger.go
@@ -6,14 +6,20 @@ import (
 )
 
 var (
-	Info  *log.Logger
-	Error *log.Logger
+	Info    *log.Logger
+	Error   *log.Logger
+	Debug   *log.Logger
+	Warning *log.Logger
 )
 
 func InitLogger() {
 	infoLog := log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
 	errorLog := log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+	debugLog := log.New(os.Stderr, "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile)
+	warnLog := log.New(os.Stderr, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
 
 	Info = infoLog
 	Error = errorLog
+	Debug = debugLog
+	Warning = warnLog
 }
